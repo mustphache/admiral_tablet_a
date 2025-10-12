21ab3c527fa0f11c1b9161b5577a8b0156402b9c
Mustapha <cybercafe.che@gmail.com>
2025-10-12 09:18:17 +0100

    [auto] sync: 2025-10-12 09:18:17

--- lib/core/money/money.dart
+++ lib/core/money/money.dart
@@ -1,5 +1,6 @@
 // lib/core/money/money.dart
 class Money {
+  /// تنسيق أرقام المال: 12,345.67
   static String fmt(double v) {
     final s = v.toStringAsFixed(2);
     return s.replaceAllMapped(RegExp(r'\B(?=(\d{3})+(?!\d))'), (m) => ',');
