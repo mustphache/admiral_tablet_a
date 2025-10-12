848871aca19f7892b3e3cb84fb79a00d720b7933
Mustapha <cybercafe.che@gmail.com>
2025-10-12 09:18:27 +0100

    [auto] sync: 2025-10-12 09:18:27

--- lib/core/time/time_formats.dart
+++ lib/core/time/time_formats.dart
@@ -1,5 +1,6 @@
 // lib/core/time/time_formats.dart
 class TimeFmt {
+  /// معرّف تاريخ yyyy-MM-dd (نستعمله كـ dayId عندما اليوم مغلق)
   static String dayIdToday() =>
-      DateTime.now().toIso8601String().split('T').first; // yyyy-MM-dd
+      DateTime.now().toIso8601String().split('T').first;
 }
