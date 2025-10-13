a7feb4f5bc0e6d8f7887a4540ca50361215f02f5
Mustapha <cybercafe.che@gmail.com>
2025-10-13 18:08:46 +0100

    [auto] sync: 2025-10-13 18:08:46

--- lib/features/wallet/screens/wallet_screen.dart
+++ lib/features/wallet/screens/wallet_screen.dart
@@ -2,7 +2,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:admiral_tablet_a/state/controllers/wallet_controller.dart';
-import 'package:admiral_tablet_a/data/models/wallet_movement.dart';
+
 
 class WalletScreen extends StatelessWidget {
   const WalletScreen({super.key});
