5c4e4f8b7c2884121ce661e2944c6730777521f5
Mustapha <cybercafe.che@gmail.com>
2025-10-13 17:28:14 +0100

    [auto] sync: 2025-10-13 17:28:13

--- lib/core/session/day_session_model.dart
+++ lib/core/session/day_session_model.dart
@@ -1,6 +1,26 @@
-// lib/core/session/day_session_model.dart
-// إعادة تعريف DaySessionState للنموذج الرسمي
-
 import 'package:admiral_tablet_a/data/models/day_session_model.dart';
 
-typedef DaySessionState = DaySessionModel;
+// نموذج الحالة اليومية (يحوي دوال فتح/غلق اليوم)
+class DaySessionState extends DaySessionModel {
+  DaySessionState({
+    required super.id,
+    required super.openedAt,
+    super.closedAt,
+    required super.openedNow,
+    super.capital,
+  });
+
+  bool get closed => closedAt != null;
+
+  static DaySessionState fromMap(Map<String, dynamic> map) {
+    return DaySessionState(
+      id: map['id'] as String,
+      openedAt: DateTime.parse(map['openedAt']),
+      closedAt: map['closedAt'] == null
+          ? null
+          : DateTime.parse(map['closedAt']),
+      openedNow: map['openedNow'] as bool,
+      capital: (map['capital'] as num?)?.toDouble(),
+    );
+  }
+}
