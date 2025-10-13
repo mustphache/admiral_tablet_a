5ca2445186d98ca22fc7c244375a7aad02c63c50
Mustapha <cybercafe.che@gmail.com>
2025-10-13 17:15:40 +0100

    [auto] sync: 2025-10-13 17:15:40

---
+++ lib/core/session/day_session_model.dart
@@ -0,0 +1,5 @@
+// lib/core/session/day_session_model.dart
+// Shim: نحافظ على الاسم القديم DaySessionState كاسم مستعار للنموذج الرسمي
+import 'package:admiral_tablet_a/data/models/day_session_model.dart';
+
+typedef DaySessionState = DaySessionModel;
