8514c7cd3507a45c90b721c85edd6b8f7c14ad56
Mustapha <cybercafe.che@gmail.com>
2025-10-13 18:16:06 +0100

    [auto] sync: 2025-10-13 18:16:06

--- lib/ui/app_routes.dart
+++ lib/ui/app_routes.dart
@@ -1,47 +1,15 @@
+// lib/ui/app_routes.dart
 import 'package:flutter/material.dart';
-import 'package:admiral_tablet_a/features/wallet/screens/wallet_movements_screen.dart';
-import 'package:admiral_tablet_a/features/wallet/screens/add_wallet_movement_screen.dart';
 
-// الشاشات
-import 'package:admiral_tablet_a/features/auth/screens/login_screen.dart';
-import 'package:admiral_tablet_a/features/home/screens/home_screen.dart';
-import 'package:admiral_tablet_a/features/day_session/day_session_screen.dart';
-import 'package:admiral_tablet_a/features/day_session/purchases_screen.dart';
-import 'package:admiral_tablet_a/features/day_session/expenses_screen.dart';
-import 'package:admiral_tablet_a/features/wallet/screens/wallet_screen.dart';
-import 'package:admiral_tablet_a/features/wallet/screens/wallet_movements_screen.dart';
-import 'package:admiral_tablet_a/features/wallet/screens/add_wallet_movement_screen.dart';
+import '../features/wallet/screens/wallet_screen.dart';
+import '../features/wallet/screens/wallet_movements_screen.dart';
+import '../features/wallet/screens/add_wallet_movement_screen.dart';
 
-class AppRoutes {
-  static const login = '/login';
-  static const home = '/home';
-  static const daySession = '/day_session';
-  static const purchases = '/purchases';
-  static const expenses = '/expenses';
-  static const wallet = '/wallet';
-  static const walletMovements = '/wallet/movements';
-  static const addWalletMovement = '/wallet/add_movement';
+// أضف بقية الشاشات حسب مشروعك...
 
-  static Route<dynamic> onGenerateRoute(RouteSettings settings) {
-    switch (settings.name) {
-      case login:
-        return MaterialPageRoute(builder: (_) => const LoginScreen());
-      case home:
-        return MaterialPageRoute(builder: (_) => const HomeScreen());
-      case daySession:
-        return MaterialPageRoute(builder: (_) => const DaySessionScreen());
-      case purchases:
-        return MaterialPageRoute(builder: (_) => const PurchasesScreen());
-      case expenses:
-        return MaterialPageRoute(builder: (_) => const ExpensesScreen());
-      case wallet:
-        return MaterialPageRoute(builder: (_) => const WalletScreen());
-      case walletMovements:
-        return MaterialPageRoute(builder: (_) => const WalletMovementsScreen());
-      case addWalletMovement:
-        return MaterialPageRoute(builder: (_) => const AddWalletMovementScreen());
-      default:
-        return MaterialPageRoute(builder: (_) => const HomeScreen());
-    }
-  }
-}
+final Map<String, WidgetBuilder> appRoutes = {
+  '/wallet': (c) => const WalletScreen(),
+  '/wallet/movements': (c) => const WalletMovementsScreen(),
+  '/wallet/movements/add': (c) => const AddWalletMovementScreen(),
+  // ...
+};
