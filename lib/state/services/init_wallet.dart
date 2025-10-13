9216b2987f27fc5a0e08ea7c3bfd28621eadda57
Mustapha <cybercafe.che@gmail.com>
2025-10-13 15:59:33 +0100

    [auto] sync: 2025-10-13 15:59:33

--- lib/state/services/init_wallet.dart
+++ lib/state/services/init_wallet.dart
@@ -1,1 +1,16 @@
-init_wallet.dart
+import 'package:admiral_tablet_a/data/db/wallet_repository.dart';
+import 'package:admiral_tablet_a/data/db/wallet_repository_memory.dart';
+import 'package:admiral_tablet_a/state/services/wallet_service.dart';
+
+class Services {
+  Services._();
+  static final Services I = Services._();
+
+  late final IWalletRepository walletRepo;
+  late final WalletService wallet;
+
+  void init() {
+    walletRepo = WalletRepositoryMemory(); // بدّل لاحقًا بـ Isar/SQLite
+    wallet = WalletService(walletRepo);
+  }
+}
